06839f99a4907a0b4cad27383e321f0dca016309
Ciaddie Rutelionis <ciaddierutelionis@Ciaddies-MacBook-Pro.local>
2023-05-25 15:41:17 -0700

    oops initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace is_ciaddie_employed;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
